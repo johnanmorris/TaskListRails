ea9992446280a4e1f36e4386e636eeffcfd9b9a4
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-27 16:26:46 -0700

    initial root view completed with test data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
   def index
+    @example_tasks = [
+      {
+        name: "SHOPPING",
+        description: "I need milk, butter, and cheese.",
+        completion_status: "incomplete",
+        completion_date: "NA"
+      },
+      {
+        name: "HOMEWORK",
+        description: "Read Act I of Hamlet",
+        completion_status: "in progress",
+        completion_date: "NA"
+      },
+      {
+        name: "DOG CARE",
+        description: "take dog to the vet",
+        completion_status: "complete",
+        completion_date: "September 27, 2016"
+      },
+    ]
   end
 
   # def show
