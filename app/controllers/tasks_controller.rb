6884b54740136a01da4d5862f7a492dfb7b443a0
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-27 17:02:43 -0700

    WIP end of day: show view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,26 +2,60 @@ class TasksController < ApplicationController
   def index
     @example_tasks = [
       {
-        name: "SHOPPING",
-        description: "I need milk, butter, and cheese.",
-        completion_status: "incomplete",
-        completion_date: "NA"
+        "name" => "SHOPPING",
+        "description" => "I need milk, butter, and cheese.",
+        "completion status" => "incomplete",
+        "completion date" => "NA"
       },
       {
-        name: "HOMEWORK",
-        description: "Read Act I of Hamlet",
-        completion_status: "in progress",
-        completion_date: "NA"
+        "name" => "HOMEWORK",
+        "description" => "Read Act I of Hamlet",
+        "completion status" => "in progress",
+        "completion date" => "NA"
       },
       {
-        name: "DOG CARE",
-        description: "take dog to the vet",
-        completion_status: "complete",
-        completion_date: "September 27, 2016"
+        "name" => "DOG CARE",
+        "description" => "take dog to the vet",
+        "completion status" => "complete",
+        "completion date" => "September 27, 2016"
       },
     ]
   end
 
-  # def show
-  # end
+  def show
+    example_tasks = [
+      {
+        "name" => "SHOPPING",
+        "description" => "I need milk, butter, and cheese.",
+        "completion status" => "incomplete",
+        "completion date" => "NA"
+      },
+      {
+        "name" => "HOMEWORK",
+        "description" => "Read Act I of Hamlet",
+        "completion status" => "in progress",
+        "completion date" => "NA"
+      },
+      {
+        "name" => "DOG CARE",
+        "description" => "take dog to the vet",
+        "completion status" => "complete",
+        "completion date" => "September 27, 2016"
+      },
+    ]
+
+    begin
+      task_id = Integer(params[:id])
+      if task_id >= 0 && task_id < example_tasks.length
+        @task = example_tasks[task_id]
+      else
+        @task = "Invalid id"
+        render status: 404
+      end
+    rescue ArgumentError
+      render status: 404
+      @task = "Invalid id"
+    end
+    puts "What is the task? #{@task}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   get '/tasks' => 'tasks#index'
-  # get '/tasks/:id' => 'tasks#show'
+  get '/tasks/:id' => 'tasks#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
