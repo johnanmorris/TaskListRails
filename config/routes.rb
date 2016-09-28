351d1bf4b64cfe13c60084e5b6df514f6980348d
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-28 14:51:46 -0700

    fixed typo in controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
   get '/tasks' => 'tasks#index'
   get '/tasks/:id' => 'tasks#show'
-  get 'tasts/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
