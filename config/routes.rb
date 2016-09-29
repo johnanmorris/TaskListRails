dab387ad3dd3baae11f7ac066b4cf0ffc6624f82
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-29 15:45:33 -0700

    added edit and update actions and views for Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   post '/tasks/' => 'tasks#create'
 
+  get '/tasks/:id/edit' => 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch '/tasks/:id' => 'tasks#update'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
