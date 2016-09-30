d6b1bf62a2bb25a9863a769d9b6e9eef884e446b
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-30 14:39:37 -0700

    added completed button functionality

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def formatted_time(task)
+    if task.completed_at != nil
+      return task.completed_at.strftime("%B %e, %Y, %I:%M:%S %p")
+    else
+      return "not yet completed"
+    end
+  end
+
+
 end
