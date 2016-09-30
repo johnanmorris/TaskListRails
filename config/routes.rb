3f3fa651e0298e38d95b359ba3ad549d2534d26a
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-30 10:37:20 -0700

    removed redundant route; modified delete route and action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   root 'tasks#index'
+
   get '/tasks' => 'tasks#index'
   get '/tasks/new' => 'tasks#new'
-
-  delete '/tasks/:id' => 'tasks#destroy', as: 'task_delete'
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   post '/tasks/' => 'tasks#create'
 
-  get '/tasks/:id/edit' => 'tasks#edit'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
   patch '/tasks/:id' => 'tasks#update'
 
-
+  delete '/tasks/:id' => 'tasks#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
