0018cc7c8d7b47e9d04fcfbc90c25c59eec40edc
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-04 20:01:24 -0700

    refactored for resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-
-  get '/tasks' => 'tasks#index'
-  get '/tasks/new' => 'tasks#new'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  post '/tasks/' => 'tasks#create'
-
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
-  patch '/tasks/:id' => 'tasks#update'
-
-  delete '/tasks/:id' => 'tasks#destroy'
+  resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
